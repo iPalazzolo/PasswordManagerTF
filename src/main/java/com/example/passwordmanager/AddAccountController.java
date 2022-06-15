478cb6abf53f7f7f4d7a3be6a3357c521ea1da3e
iPalazzolo <ignacio.palazzolo@hotmail.com>
2022-06-15 14:56:29 -0300

    Nueva funcionalidad a la ventana 'Agregar Cuenta' (Botón Cancelar)

--- src/main/java/com/example/passwordmanager/AddAccountController.java
+++ src/main/java/com/example/passwordmanager/AddAccountController.java
@@ -1,5 +1,16 @@
 package com.example.passwordmanager;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class AddAccountController {
 
+    @FXML
+    private Button cancelarButton;
+    public void cancelarButtonOnAction(ActionEvent event) {
+        Stage stage = (Stage) cancelarButton.getScene().getWindow();
+        stage.close();
+    }
 }
