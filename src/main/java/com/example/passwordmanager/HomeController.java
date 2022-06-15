ea422b651059780c27f581231065d5add8c010a0
iPalazzolo <ignacio.palazzolo@hotmail.com>
2022-06-15 14:56:09 -0300

    Nueva funcionalidad a la ventana 'Home' (Botón Agregar Cuenta)

--- src/main/java/com/example/passwordmanager/HomeController.java
+++ src/main/java/com/example/passwordmanager/HomeController.java
@@ -1,16 +1,26 @@
 package com.example.passwordmanager;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class HomeController {
-    @FXML
-    private Label bienvenidoNombreLabel;
-    @FXML
-    private Label bienvenidoApellidoLabel;
 
-    public void usuarioActual() {
-        //DBConnection conec
-        //bienvenidoNombreLabel.setText();
+    public void addAccountButtonOnAction(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("addAccount.fxml"));
+            Stage agregarCuentaStage = new Stage();
+            Scene scene = new Scene(fxmlLoader.load(), 450, 350);
+            agregarCuentaStage.setTitle("Agregar Nueva Cuenta");
+            agregarCuentaStage.setScene(scene);
+            agregarCuentaStage.setResizable(false);
+            agregarCuentaStage.show();
+
+        } catch(Exception e) {
+            e.printStackTrace();
+            e.getCause();
+        }
     }
+
 }
